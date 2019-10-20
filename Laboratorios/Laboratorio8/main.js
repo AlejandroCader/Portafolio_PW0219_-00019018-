4992d8b549104de785ed80028a0c3faf28905373
miguel.ccader@gmail.com <miguel.ccader@gmail.com>
2019-10-20 00:04:17 -0600

    Adding excercise to Laboratorio8

--- Laboratorios/Laboratorio8/main.js
+++ Laboratorios/Laboratorio8/main.js
@@ -1,9 +1,3 @@
-let carnet_field = document.querySelector("#carnet_field");
-let schedule_dropdown = document.querySelector("#schedule_field");
-let late_switch = document.querySelector("#late_switch");
-let submit_btn = document.querySelector("#submit_btn");
-let table_body = document.querySelector("#table_body");
-let carnet_regex = new RegExp("[0-9]{8}");
 var rows = [];
 var counterId = 0;
 
@@ -74,29 +68,37 @@ let parseLateSwitch = value => {
   }
   return "A tiempo";
 };
+window.onload = function() {
+  var carnet_field = document.querySelector("#carnet_field");
+  var schedule_dropdown = document.querySelector("#schedule_field");
+  var late_switch = document.querySelector("#late_switch");
+  var submit_btn = document.querySelector("#submit_btn");
+  var table_body = document.querySelector("#table_body");
+  var carnet_regex = new RegExp("[0-9]{8}");
 
-submit_btn.addEventListener("click", () => {
-  let carnet = carnet_field.value;
-  let schedule =
-    schedule_dropdown.options[schedule_dropdown.selectedIndex].text;
-  let late = parseLateSwitch(late_switch.checked);
+  submit_btn.addEventListener("click", () => {
+    let carnet = carnet_field.value;
+    let schedule =
+      schedule_dropdown.options[schedule_dropdown.selectedIndex].text;
+    let late = parseLateSwitch(late_switch.checked);
 
-  if (carnet_regex.test(carnet)) {
-    addStudent(carnet, schedule, late);
-  } else {
-    alert("Formato de carnet no válido");
-  }
-});
+    if (carnet_regex.test(carnet)) {
+      addStudent(carnet, schedule, late);
+    } else {
+      alert("Formato de carnet no válido");
+    }
+  });
 
-carnet_field.addEventListener("keyup", event => {
-  let keyCode = event.keyCode;
-  let carnet = carnet_field.value;
-  if (keyCode == 13) {
-    submit_btn.click();
-  }
-  if (carnet_regex.test(carnet)) {
-    submit_btn.disabled = false;
-  } else {
-    submit_btn.disabled = true;
-  }
-});
+  carnet_field.addEventListener("keyup", event => {
+    let keyCode = event.keyCode;
+    let carnet = carnet_field.value;
+    if (keyCode == 13) {
+      submit_btn.click();
+    }
+    if (carnet_regex.test(carnet)) {
+      submit_btn.disabled = false;
+    } else {
+      submit_btn.disabled = true;
+    }
+  });
+};
